448e9d5b028a47ea1f3b799e3c26e09f0b365dc9
SpacePistache <lukeholley@hotmail.com>
2024-12-10 14:35:28 +0100

    LF endings for Betty

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - Creates a new binary tree node on the right
+ * @parent: A pointer to the parent node
+ * @value: The value to store in the new node
+ *
+ * Return: A pointer to the new node, or NULL on failure
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+}
