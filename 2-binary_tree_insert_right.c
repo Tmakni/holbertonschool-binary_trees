ae992d16ef048d9290e8e1fa9d7c000efb4d8d3d
SpacePistache <lukeholley@hotmail.com>
2024-12-10 13:18:07 +0100

    2-right fixed node being overwritten

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,17 +11,18 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node_right = malloc(sizeof(binary_tree_t));
 
-if (parent == NULL)
+
+if (new_node_right == NULL || parent == NULL )
 return (NULL);
 
 new_node_right->n = value;
-new_node_right->parent = parent;
 new_node_right->right = NULL;
 new_node_right->left = NULL;
+new_node_right->parent = parent;
 
 if (parent->right != NULL)
 {
-new_node_right = parent->right;
+new_node_right->right = parent->right;
 parent->right->parent = new_node_right;
 }
 
