d86dccb36a2d6111c018919dc1fdbf61036c8d2e
SpacePistache <lukeholley@hotmail.com>
2024-12-11 19:47:15 +0100

    18-uncle forgot prototype in headerfile

--- binary_trees.h
+++ binary_trees.h
@@ -46,6 +46,6 @@ size_t binary_tree_height(const binary_tree_t *tree);
 
 size_t binary_tree_size(const binary_tree_t *tree);
 
-int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif
