a71da37af8b5a2be4c0bdfd279c43dc8ac950f47
SpacePistache <lukeholley@hotmail.com>
2024-12-11 12:57:22 +0100

    14-balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - Calculates whether binary tree is balanced
+ * @tree: Pointer to root of tree.
+ *
+ * Return: result of left height minus right height
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+
+if (tree == NULL)
+return (0);
+
+int left_height = binary_tree_height(tree->left);
+int right_height = binary_tree_height(tree->right);
+
+return (1 + (left_height > right_height ? left_height : right_height));
+
+if (tree == NULL)
+return (0);
+
+int left_height = binary_tree_height(tree->left);
+int right_height = binary_tree_height(tree->right);
+
+return (left_height - right_height);
+}
