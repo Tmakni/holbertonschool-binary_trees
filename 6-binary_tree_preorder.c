48d21882ce23a176d5fe16db5bac3a173a42613b
SpacePistache <lukeholley@hotmail.com>
2024-12-11 09:21:40 +0100

    6-pre betty styling added

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * binary_tree_insert_right - Creates a new binary tree node on the right
- * @parent: A pointer to the parent node
- * @value: The value to store in the new node
+ * binary_tree_preorder - Traverses the tree in preorder
+ * @tree: Pointer to root of tree.
+ * @func: The pointer that moves through the nodes
  *
- * Return: A pointer to the new node, or NULL on failure
+ * Return: content of each node
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 
 if (tree == NULL)
-return; 
+return;
 
 func(tree->n);
 binary_tree_preorder(tree->left, func);
