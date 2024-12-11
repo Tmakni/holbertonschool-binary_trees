28be2a7b976da1c11518d66f772b131820ee974b
SpacePistache <lukeholley@hotmail.com>
2024-12-11 09:14:57 +0100

    6-pre

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -15,6 +15,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 if (tree == NULL)
 return; 
 
+func(tree->n);
 printf("%d\n", tree->n);
 binary_tree_preorder(tree->left, func);
 binary_tree_preorder(tree->right, func);
