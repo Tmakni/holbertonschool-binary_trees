2e3e440894f45026213fac29447ae28c961f9205
SpacePistache <lukeholley@hotmail.com>
2024-12-11 19:21:23 +0100

    14-balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,15 +8,32 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+size_t left_height, right_height;
+
 if (tree == NULL)
 return (0);
-
-int left_height;
-int right_height;
 
 left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
 
+return ((left_height > right_height ? left_height : right_height) +1);
+}
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure the balance factor.
+ *
+ * Return: The balance factor, or 0 if tree is NULL.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+int left_height, right_height;
+
+if (tree == NULL)
+return (0);
+
+left_height = (int)binary_tree_height(tree->left);
+right_height = (int)binary_tree_height(tree->right);
 
 return (left_height - right_height);
 }
