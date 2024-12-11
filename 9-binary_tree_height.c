c5727d6ab7c255eba3460034aac0287c9685600a
Your Name <you@example.com>
2024-12-11 13:17:44 +0000

    9 ok

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,9 +14,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	(tree->left == NULL &&
 	tree->right == NULL &&
 	tree->parent == NULL))
-		{
-			return (0);
-		}
+	{
+		return (0);
+	}
+
 	else if (tree->parent != NULL)
 	{
 		l_height = binary_tree_height(tree->left);
