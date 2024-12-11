d1d49e589da01427af9b6e7a9938d0787c61bb9c
SpacePistache <lukeholley@hotmail.com>
2024-12-11 11:33:18 +0100

    12-leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_depth - Returns the depth of nodes
+ * @tree: Pointer to root of tree.
+ *
+ * Return: content of each node
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{ 
+if (tree == NULL)
+return (0);
+
+if (tree->left == NULL || tree->right == NULL)
+return (1);
+
+return binary_tree_leaves(tree->left) + binary_tree_leaves (tree->right);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -40,6 +40,6 @@ void binary_tree_delete(binary_tree_t *tree);
 
 int binary_tree_is_root(const binary_tree_t *node);
 
-size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
 
 #endif
