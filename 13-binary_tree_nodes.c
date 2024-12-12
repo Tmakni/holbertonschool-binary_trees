09c75c71db2619f40a1314f3b1c8d4aa0754f73b
Your Name <you@example.com>
2024-12-12 11:34:28 +0000

    13 ok

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "13-main.c"
+#include "binary_trees.h"
 
 /**
  * binary_tree_nodes - Counts the number of leaves
@@ -22,8 +23,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		count++;
 	}
 
-	 binary_tree_nodes(tree->left);
-	 binary_tree_nodes(tree->right);
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
 
 	return (count);
 }
